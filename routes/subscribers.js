3b9b2283707d083b887246f138ea98539290c6b9
Kyle Bautista <inserhere@gmail.com>
2021-03-24 15:06:06 -0700

    get all routes added

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,9 +2,29 @@ const express = require("express");
 const router = express.Router();
 
 //Route for Getting all Subscribers
+router.get('/',(req,res)=>{
+    res.send("hello world"); //testing with rest client extension
+});
+
 //Route getting One
+router.get('/:id',(req,res)=>{
+    res.send(req.params.id);
+});
+
 //Route for creating One
+router.post('/',(req,res)=>{
+    
+});
+
 //Route for updating one
+//put updates all the information, patch updates the things you want to change
+router.patch('/:id',(req,res)=>{
+    
+});
+
 //Route for deleting one.
+router.delete('/:id',(req,res)=>{
+    
+});
 
 module.exports = router;
